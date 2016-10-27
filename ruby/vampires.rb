0ff2d00e3d940b55d035950632b8dca5eaf7c1cf
Mhar Andal <mharandal96@gmail.com>
2016-10-27 11:08:29 -0500

    User input added to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,20 @@
+#USER INPUT
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?(yes or no)"
+response = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?(yes or no)"
+enroll = gets.chomp
+
+
+
+
