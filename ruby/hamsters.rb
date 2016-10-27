1360afbf04048a9a5d2ea7f0cc04c16b999965ce
Mhar Andal <mharandal96@gmail.com>
2016-10-27 17:54:09 -0500

    Add User Input

---
+++ ruby/hamsters.rb
@@ -0,0 +1,15 @@
+#USER INPUT
+puts "Name: "
+name = gets.chomp
+
+puts "Volume Level: "
+volume = gets.chomp.to_i
+
+puts "Fur Color: "
+color = gets.chomp
+
+puts "Good for Adoption?(yes or no): "
+adopt = gets.chomp
+
+puts "Age: "
+age = gets.chomp.to_i
