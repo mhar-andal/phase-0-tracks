c1aadeef39bdfd6c78ff65731af4c12d84df6c5c
Mhar Andal <mharandal96@gmail.com>
2016-10-30 09:35:48 -0500

    Testing hash input

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -2,13 +2,17 @@
 Pseudocode
 - Prompt questions for user input
 - Store user input using hashes
-- 
+- Print hash back to display
+- Prompt the user if they need to update
+- - If they need to update, ask for the variable they need to update.
+- Print hash and exit
 =end
 
 client_data = Hash.new(0)
 
 puts "Enter your full name: "
 name = gets.chomp
+
 
 client_data[:name] = name
 p client_data
