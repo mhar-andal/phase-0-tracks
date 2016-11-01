e4c2167436bf20ffc844a8a27a277c26020ad96f
Mhar Andal <mharandal96@gmail.com>
2016-10-31 21:49:41 -0500

    Psuedocode for alias

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,14 @@
+=begin
+	
+PSUEDOCODE
+-Take user input for first and last name
+-call method and pass first and last name in parameters
+-run a condition to check for uppercase, if uppercase, downcase and set back to uppercase when done
+ --user hash to store value of uppercase
+-run a condition to check for vowels, if vowel found increment to next vowel. 
+-run a condition to check if next consonants is a vowel, if vowel, skip vowel and choose next consonant
+
+	
+=end
+
+
