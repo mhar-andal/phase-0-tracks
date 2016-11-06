32b29a8b7b44f33406010bdcd8786abb6bfbb39c
Mhar Andal <mharandal96@gmail.com>
2016-11-06 17:45:39 -0600

    Finish 6.4 Release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,32 @@
+module Shout
+	def chirp(int)
+		int.times do 
+			puts "Chirp!"
+		end
+	end
+	
+	def jumpForJoy
+		puts "*jumping for joy*"
+	end
+end
 
+class Bird
+	include Shout
+end
+
+class Cricket
+	include Shout
+end
+
+bird = Bird.new
+bird.chirp(1)
+bird.jumpForJoy
+
+cricket = Cricket.new
+cricket.chirp(3)
+cricket.jumpForJoy
+
+=begin
 module Shout
 	def self.yell_angrily(words)
 		words = words + "!!!" + " :("
@@ -13,3 +41,4 @@ end
 
 Shout.yell_angrily("Hello")
 Shout.yelling_happily("Happy")
+=end
