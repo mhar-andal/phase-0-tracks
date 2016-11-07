2873e1e89662e03fb2941c59852c19dd6017a709
Mhar Andal <mharandal96@gmail.com>
2016-11-06 19:47:00 -0600

    Add guess method to WordGame

--- ruby/Game/wordguesser.rb
+++ ruby/Game/wordguesser.rb
@@ -9,6 +9,19 @@ class WordGame
 		@game_over = false
 	end
 
+	def guess(char)
+		if @word.include?(char)
+			index = 0
+			@split_word.length.times do 
+				if @split_word[index] == char
+					@secret_word[index] = char
+				end		
+			index += 1
+			end
+		end
+
+		@guess_count += 1
+	end
 end
 
 
