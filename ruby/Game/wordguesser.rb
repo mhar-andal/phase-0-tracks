72e03495d4fbd82df2944829723f0c93a8a1d950
Mhar Andal <mharandal96@gmail.com>
2016-11-06 19:44:14 -0600

    Add initialize method for class WordGame

--- ruby/Game/wordguesser.rb
+++ ruby/Game/wordguesser.rb
@@ -1,5 +1,14 @@
 class WordGame
-	
+	attr_reader :word, :secret_word
+
+	def initialize(word)
+		@word = word
+		@split_word = word.split('')
+		@secret_word = word.split('').each {|x| x = '_' }
+		@guess_count = 0
+		@game_over = false
+	end
+
 end
 
 
