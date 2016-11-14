1015008ddb81222f64bd54adc350fe8d29105d84
Mhar Andal <mharandal96@gmail.com>
2016-11-14 17:30:12 -0600

    Add psuedocode for explore.js

---
+++ js/explore.js
@@ -0,0 +1,5 @@
+// Psuedocode 
+// function that takes string as a parameter and reverses the string
+//for each letter in the string going backwards
+// add the letter to a new string
+// return new string
