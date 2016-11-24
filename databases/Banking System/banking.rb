8f66a12752c1bc454da336e206d8dc81cd0874ad
Mhar Andal <mharandal96@gmail.com>
2016-11-23 18:00:33 -0600

    Add account creation with data save

--- databases/Banking System/banking.rb
+++ databases/Banking System/banking.rb
@@ -1,32 +1,27 @@
-# require 'sqlite3'
-# require 'faker'
+require 'sqlite3'
+require 'faker'
 
-# bank_db = SQLite3::Database.new("bank_info.db")
-# db.results_as_hash = true
+bank_db = SQLite3::Database.new("bank_info.db")
+bank_db.results_as_hash = true
 
-# create_table_cmd = <<-SQL
-#   CREATE TABLE IF NOT EXISTS kittens(
-#     acct_num INT,
-#     name VARCHAR(255),
-#     password VARCHAR(255),
-#     balance DECIMAL,
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS accounts(
+    acct_num INT,
+    name VARCHAR(255),
+    password VARCHAR(255),
+    balance DECIMAL
+  )
+SQL
 
-#   )
-# SQL
-
-# bank_db.execute(create_table_cmd)
-
-# def create_kitten(db, name, age)
-#   db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
-# end
+bank_db.execute(create_table_cmd)
 
 class BankingAccount
 
-	def initialize(fullname)
-		@fullname = fullname
-		@password = newpassword
-		@balance = 100
-		@accountNumber = genAccountNumber
+	def initialize(args)
+		@fullname = args[:fullname]
+		@password = args[:password] || newpassword
+		@balance = args[:balance] || 100
+		@accountNumber = args[:acct_num] || genAccountNumber
 	end
 
 	def genAccountNumber
@@ -40,18 +35,22 @@ class BankingAccount
 		puts "Account Number: #{@accountNumber}"
 	end
 
+	def create_account(bank_db)
+ 		bank_db.execute("INSERT INTO accounts (acct_num, name, password, balance) VALUES (?, ?, ?, ?)", [@accountNumber, @fullname, @password, @balance])
+	end
+
 	private
 
 	def newpassword
 		confirmed = false
 		while confirmed == false
 			p "Enter Password: "
-			password = gets.chomp
-			p "Confirm your password: "
 			confirm_password = gets.chomp
-			password == confirm_password ? confirmed = true :  password_msg
+			p "Confirm your password: "
+			confirm_password_valid = gets.chomp
+			confirm_password == confirm_password_valid ? confirmed = true :  password_msg
 		end
-		return password
+		return confirm_password
 	end
 
 	def password_msg
@@ -59,10 +58,18 @@ class BankingAccount
 		puts "Passwords do not match!"
 		puts
 	end
-
-
 end
 
+def new_account(bank_db)
+	puts "We are glad you're starting a banking account with us!"
+	puts "First, we need your full name!"
+	p "Full Name: "
+	fullname = gets.chomp
+	puts
+	account = BankingAccount.new({fullname: fullname})
+	account.printInfo
+	account.create_account(bank_db)
+end
 
 #DRIVER CODE
 puts "Welcome to the Bank of Ruby!"
@@ -76,16 +83,9 @@ loop do
 	choice = gets.chomp.to_i
 	case choice 
 		when 1
-			puts "We are glad you're starting a banking account with us!"
-			puts "First, we need your full name!"
-			p "Full Name: "
-			fullname = gets.chomp
-			puts
-			account = BankingAccount.new(fullname)
-			account.printInfo
-
+			new_account(bank_db)
 		when 2
-
+			puts "Enter you"
 		when 3
 
 		when 4
